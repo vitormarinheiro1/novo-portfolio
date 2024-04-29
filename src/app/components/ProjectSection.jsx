f627dea1bfee96bd97d4d6f4ef0dcf4506e4c0e4
Suporte <suporte@HBR.LOCAL>
2024-04-29 16:47:37 -0300

    Section My Projects finalizada!

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import ProjectCard from './ProjectCard';
+import ProjectTag from './ProjectTag';
 
 const projectsData = [
     {
@@ -59,14 +61,43 @@ const projectsData = [
 ];
 
 const ProjectSection = () => {
+    const [tag, setTag] = useState("All");
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag);
+    };
+
+    const filteredProjects = projectsData.filter((project) =>
+        project.tag.includes(tag)
+    );
+
     return (
         <>
-            <h2>My Projects</h2>
-            <div>
-                {projectsData.map((project) => <ProjectCard
+            <h2 className="text-center text-4xl font-bold text-white mt-4 mb-6">My Projects</h2>
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="All"
+                    isSelected={tag === "All"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Web"
+                    isSelected={tag === "Web"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Mobile"
+                    isSelected={tag === "Mobile"}
+                />
+            </div>
+            <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {filteredProjects.map((project) => <ProjectCard
                     key={project.id}
                     title={project.title} description={project.description}
                     imgUrl={project.image}
+                    gitUrl={project.gitUrl}
+                    previewUrl={project.previewUrl}
                 />
                 )}
             </div>
