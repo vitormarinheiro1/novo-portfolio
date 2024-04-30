2383bf34cfdf1747b51eab0b6f8d8e7041aa1b77
Suporte <suporte@HBR.LOCAL>
2024-04-30 17:40:28 -0300

    Adicionando Section Achievements e Section Email

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,11 +8,11 @@ import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
-        <section>
+        <section className="lg:py-16">
             <div className="grid grid-cols-1 sm:grid-cols-12 text-center">
-                <div className="col-span-7 place-self-center text-center sm:text-left">
-                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                <div className="col-span-8 place-self-center text-center sm:text-left justify-center-start">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl ld:leading-normal font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
                             Hello, I'm{" "}
                         </span>
                         <br></br>
@@ -23,7 +23,7 @@ const HeroSection = () => {
                                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                                 'Web Developer',
                                 1000,
-                                'Mobile Developer',
+                                'Backend Developer',
                                 1000,
                                 'UI/UX Designer',
                                 1000
@@ -35,13 +35,13 @@ const HeroSection = () => {
                     </h1>
                     <p className="text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-w-500 hover:bg-slate-200 text-white">Hire me</button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-w-500 hover:bg-slate-200 text-white">Hire me</button>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
                         </button>
                     </div>
                 </div>
-                <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
                     <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
                             src="/images/hero-image.png"
